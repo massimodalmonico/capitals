99357fa77d60e88efc59f937110124644103630a
massimodalmonico <massimodalmonico@gmail.com>
2019-11-19 16:01:28 -0500

    resolved first point of issue #7

--- sysargv.py
+++ sysargv.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
-'''importing the module checks from capitalspackage 
-inside check module there are the two functions 
+'''importing the module checks from capitalspackage. 
+inside checks module there are the two functions 
 check_capital and check_state'''
 
 from capitalspackage import checks
@@ -10,11 +10,25 @@ from capitalspackage import checks
 
 import sys
 
-# store the first parameter passed by the user in chosen_input
 
-chosen_input = sys.argv[1]
+# store the first parameter passed by the user in chosen_input
+# after checking if exacly one argument has been passed by the user
+
+if len (sys.argv) == 2:
+    chosen_input = sys.argv[1]
+
 
 # perform the checks on chosen_input and return results
 
-checks.check_capital(str(chosen_input))
-checks.check_state(str(chosen_input))
+    checks.check_capital(str(chosen_input))
+    checks.check_state(str(chosen_input))
+
+
+# produce meaningful error if argument passed != 1
+
+else: print ("wrong number of arguments" +
+              "\nplease insert exacly one argument: country or capital name")
+
+
+
+
