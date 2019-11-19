3455b400d5427d300e9d2564eb05d3427c46a4cf
massimodalmonico <massimodalmonico@gmail.com>
2019-11-19 16:08:05 -0500

    resolved second point of issue #7

--- capitalspackage/checks.py
+++ capitalspackage/checks.py
@@ -60,19 +60,22 @@ list_of_capitals = {'Aland Islands':'Mariehamn',
 'United Kingdom':'London',
 'Vatican City':'Vatican City'}
 
-# function to check if the capital inserted is presented in the list
-
+# function to check if the capital inserted is present in the list
 def check_capital(state_name):
+
     if state_name in list_of_capitals:
-        print("The capital of {} is {}".format(state_name, list_of_capitals[state_name]))
-    else:
-        print("Sorry, {} does not seem to be an European state".format(state_name))
+        print("The capital of {} is {}".format(state_name,
+              list_of_capitals[state_name]))
 
-# function to check if the state inserted is presented in the list 
 
+# function to check if the state inserted is present in the list
 def check_state(capital_name):
+
     for state, capital in list_of_capitals.items():
         if capital == capital_name:
-            print("The European state whose capital is {} is {}".format(capital_name, state))
-            return
-    print("Sorry, {} is not the capital of any European state".format(capital_name))
+            print("The European state whose capital is {} is {}".
+                  format(capital_name, state))
+
+    if capital_name not in list_of_capitals and capital_name not in list_of_capitals.values():
+        print("Sorry, {} does not seem to be an European state or capital".
+              format(capital_name))
