a9ed2cf9cb93e6240736ae9b8c4df8b0a4650b00
leonardfonz <leofontana1998@gmail.com>
2019-11-28 11:38:37 -0500

    solved issue #11

--- capitalspackage/checks.py
+++ capitalspackage/checks.py
@@ -14,12 +14,23 @@ def load_csv(filename):
 def check_capital(state_name, checklist, args):
     if state_name in checklist:
         if args.verbosity >= 2:
-            print("args.name is validated by function input_validation then passed to check functions. in those the dictionary 'list_of_capitals' is checked, previously loaded by function load_csv. the match found is > ", checklist[state_name])
+            if args.returnr == True:
+                print("args.name is validated by function input_validation then passed to check functions. in those the dictionary passed as checklist is checked, previously loaded by function load_csv. the result returned is > ", checklist[state_name])
+                return checklist[state_name]
+            else:
+                print("args.name is validated by function input_validation then passed to check functions. in those the dictionary passed as checklist is checked, previously loaded by function load_csv. the match found is > ", checklist[state_name])
         elif args.verbosity >= 1:
-            print("The capital of {} is {}".format(state_name,
-              checklist[state_name]))
+            if args.returnr == True:
+                print("Returning {}, capital of {}".format(checklist[state_name], state_name))
+                return checklist[state_name]
+            else:
+                print("The capital of {} is {}".format(state_name,
+                  checklist[state_name]))
         else:
-            print(checklist[state_name])
+            if args.returnr == True:
+                return checklist[state_name]
+            else:
+                print(checklist[state_name])
 
 
 # function to check if the state inserted is present in the list
@@ -28,12 +39,22 @@ def check_state(capital_name, checklist, args):
     for state, capital in checklist.items():
         if capital == capital_name:
             if args.verbosity >= 2:
-                print("args.name is validated by function input_validation then passed to check functions. in those the dictionary 'list_of_capitals' is checked, previously loaded by function load_csv. the match found is > ", state)
+                if args.returnr == True:
+                    print("args.name is validated by function input_validation then passed to check functions. in those the dictionary passed as checklist is checked, previously loaded by function load_csv. the result returned is > ", state)
+                    return state
+                else:
+                    print("args.name is validated by function input_validation then passed to check functions. in those the dictionary passed in checklist is checked, previously loaded by function load_csv. the match found is > ", state)
             elif args.verbosity >= 1:
-                print("The European state whose capital is {} is {}".
-                  format(capital_name, state))
+                if args.returnr == True:
+                    print("Returning {} European state whose capital is {}".format(state, capital_name))
+                    return state
+                else:
+                    print("The European state whose capital is {} is {}".format(capital_name, state))
             else:
-                print (state)
+                if args.returnr == True:
+                    return state
+                else:
+                    print (state)
     if capital_name not in checklist and capital_name not in checklist.values():
         print("Sorry, {} does not seem to be an European state or capital".
               format(capital_name))
