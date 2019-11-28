6d3871a752cbdcca45182a77a2ad8c3013a29c40
saraweatherley <sara.weatherley@hotmail.it>
2019-11-28 10:30:44 -0500

    added parser to userapp.py, upgraded Capital CSV.csv, deleted main.py (useless and didn't work) and upgraded checks functions (added loadcsv function as well)

--- capitalspackage/checks.py
+++ capitalspackage/checks.py
@@ -1,21 +1,41 @@
-# function to check if the capital inserted is present in the list
-def check_capital(state_name):
+import csv
 
-    if state_name in list_of_capitals:
-        print("The capital of {} is {}".format(state_name,
-              list_of_capitals[state_name]))
+def load_csv(filename):
+
+    list_of_capitals = {}
+    with open (filename) as csvfile:
+        reader = csv.reader (csvfile, delimiter = ';')
+        for row in reader:
+            list_of_capitals[row[0]] = row[1]
+
+    return list_of_capitals
+
+# function to check if the capital inserted is present in the list
+def check_capital(state_name, checklist, args):
+    if state_name in checklist:
+        if args.verbosity >= 2:
+            print("args.name is validated by function input_validation then passed to check functions. in those the dictionary 'list_of_capitals' is checked, previously loaded by function load_csv. the match found is > ", checklist[state_name])
+        elif args.verbosity >= 1:
+            print("The capital of {} is {}".format(state_name,
+              checklist[state_name]))
+        else:
+            print(checklist[state_name])
+
 
 # function to check if the state inserted is present in the list
-def check_state(capital_name):
+def check_state(capital_name, checklist, args):
 
-    for state, capital in list_of_capitals.items():
+    for state, capital in checklist.items():
         if capital == capital_name:
-            print("The European state whose capital is {} is {}".
+            if args.verbosity >= 2:
+                print("args.name is validated by function input_validation then passed to check functions. in those the dictionary 'list_of_capitals' is checked, previously loaded by function load_csv. the match found is > ", state)
+            elif args.verbosity >= 1:
+                print("The European state whose capital is {} is {}".
                   format(capital_name, state))
-
-
-    if capital_name not in list_of_capitals and
-    capital_name not in list_of_capitals.values():
-
+            else:
+                print (state)
+    if capital_name not in checklist and capital_name not in checklist.values():
         print("Sorry, {} does not seem to be an European state or capital".
               format(capital_name))
+
+
