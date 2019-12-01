64e539dd1106e9585118dc76376ac6213ae550eb
massimodalmonico <massimodalmonico@gmail.com>
2019-12-01 14:58:43 -0500

    solved issue #13

--- capitalspackage/checks.py
+++ capitalspackage/checks.py
@@ -1,5 +1,9 @@
 import csv
 
+import requests
+
+import json
+
 def load_csv(filename):
 
     list_of_capitals = {}
@@ -60,3 +64,234 @@ def check_state(capital_name, checklist, args):
               format(capital_name))
 
 
+#function to fetch extra information about countries from REST api
+def get_country_data (base_url, countryname, args):
+
+    url= base_url.format(countryname)
+    response= requests.get(url)
+    if response.status_code == 200:
+        result = json.loads(response.text)  # parse json to dict
+
+        if args.extrainfo >= 4:
+
+            if args.verbosity >= 2:
+                if args.returnr == True:
+                    print ("succesfully fetched api data from REST Countries. returning list with population, area (km^2), currencies list and languages list of {}".format(countryname))
+                    elist=[]
+                    elistc=[]
+                    elistl=[]
+                    for r in result:
+                        elist.append(r['population'])
+                        elist.append(r['area'])
+                        for n in range(len(r['currencies'])):
+                            elistc.append(r['currencies'][n]['name'])
+                        for n in range(len(r['languages'])):
+                            elistl.append(r['languages'][n]['name'])
+                    elist.append(elistc)
+                    elist.append(elistl)
+                    return elist
+                else:
+                    print ("succesfully fetched api data from REST Countries")
+                    elistc=[]
+                    elistl=[]
+                    for r in result:
+                        print (countryname, "'s population= ", r['population'])
+                        print (countryname, "'s area (km^2) = ", r['area'])
+                        for n in range(len(r['currencies'])):
+                            elistc.append(r['currencies'][n]['name'])
+                        print (countryname, "'s currencies= ", elistc)
+                        for n in range(len(r['languages'])):
+                            elistl.append(r['languages'][n]['name'])
+                        print (countryname, "'s languages= ", elistl)
+
+            elif args.verbosity >= 1:
+                if args.returnr == True:
+                    print ("returning list with population, area (km^2), currencies list and languages list of {}".format(countryname))
+                    elist=[]
+                    elistc=[]
+                    elistl=[]
+                    for r in result:
+                        elist.append(r['population'])
+                        elist.append(r['area'])
+                        for n in range(len(r['currencies'])):
+                            elistc.append(r['currencies'][n]['name'])
+                        for n in range(len(r['languages'])):
+                            elistl.append(r['languages'][n]['name'])
+                    elist.append(elistc)
+                    elist.append(elistl)
+                    print (elist)
+                    return elist
+                else:
+                    elistc= []
+                    elistl= []
+                    for r in result:
+                        print (countryname, "'s population= ", r['population'])
+                        print (countryname, "'s area (km^2) = ", r['area'])
+                        for n in range(len(r['currencies'])):
+                            elistc.append(r['currencies'][n]['name'])
+                        print (countryname, "'s currencies= ", elistc)
+                        for n in range(len(r['languages'])):
+                            elistl.append(r['languages'][n]['name'])
+                        print (countryname, "'s languages= ", elistl)
+
+            else:
+                if args.returnr == True:
+                    elist= []
+                    elistc= []
+                    elistl= []
+                    for r in result:
+                        elist.append(r['population'])
+                        elist.append(r['area'])
+                        for n in range(len(r['currencies'])):
+                            elistc.append(r['currencies'][n]['name'])
+                        for n in range(len(r['languages'])):
+                            elistl.append(r['languages'][n]['name'])
+                    elist.append(elistc)
+                    elist.append(elistl)
+                    return elist
+                else:
+                    elistc= []
+                    elistl= []
+                    for r in result:
+                        print (r['population'])
+                        print (r['area'], " km^2")
+                        for n in range(len(r['currencies'])):
+                            elistc.append(r['currencies'][n]['name'])
+                        print (elistc)
+                        for n in range(len(r['languages'])):
+                            elistl.append(r['languages'][n]['name'])
+                        print (elistl)
+
+
+        elif args.extrainfo >= 3:
+            if args.verbosity >= 2:
+                if args.returnr == True:
+                    print ("succesfully fetched api data from REST Countries. returning list with population, area (km^2) and currencies list of {}".format(countryname))
+                    elist=[]
+                    elistc=[]
+                    for r in result:
+                        elist.append(r['population'])
+                        elist.append(r['area'])
+                        for n in range(len(r['currencies'])):
+                            elistc.append(r['currencies'][n]['name'])
+                    elist.append(elistc)
+                    return elist
+                else:
+                    print ("succesfully fetched api data from REST Countries")
+                    elistc=[]
+                    for r in result:
+                        print (countryname, "'s population= ", r['population'])
+                        print (countryname, "'s area (km^2) = ", r['area'])
+                        for n in range(len(r['currencies'])):
+                            elistc.append(r['currencies'][n]['name'])
+                        print (countryname, "'s currencies= ", elistc)
+            elif args.verbosity >= 1:
+                if args.returnr == True:
+                    print ("returning list with population, area (km^2) and currencies list of {}".format(countryname))
+                    elist=[]
+                    elistc=[]
+                    for r in result:
+                        elist.append(r['population'])
+                        elist.append(r['area'])
+                        for n in range(len(r['currencies'])):
+                            elistc.append(r['currencies'][n]['name'])
+                    elist.append(elistc)
+                    print (elist)
+                    return elist
+                else:
+                    elistc= []
+                    for r in result:
+                        print (countryname, "'s population= ", r['population'])
+                        print (countryname, "'s area (km^2) = ", r['area'])
+                        for n in range(len(r['currencies'])):
+                            elistc.append(r['currencies'][n]['name'])
+                        print (countryname, "'s currencies= ", elistc)
+
+            else:
+                if args.returnr == True:
+                    elist= []
+                    elistc= []
+                    for r in result:
+                        elist.append(r['population'])
+                        elist.append(r['area'])
+                        for n in range(len(r['currencies'])):
+                            elistc.append(r['currencies'][n]['name'])
+                    elist.append(elistc)
+                    return elist
+                else:
+                    elistc= []
+                    for r in result:
+                        print (r['population'])
+                        print (r['area'], " km^2")
+                        for n in range(len(r['currencies'])):
+                            elistc.append(r['currencies'][n]['name'])
+                        print (elistc)
+
+        elif args.extrainfo >= 2:
+            if args.verbosity >= 2:
+                if args.returnr == True:
+                    print ("succesfully fetched api data from REST Countries. returning list with population and area (km^2) of {}".format(countryname))
+                    elist=[]
+                    for r in result:
+                        elist.append(r['population'])
+                        elist.append(r['area'])
+                    return elist
+                else:
+                    print ("succesfully fetched api data from REST Countries")
+                    for r in result:
+                        print (countryname, "'s population = ", r['population'])
+                        print (countryname, "'s area (km^2) = ", r['area'])
+            elif args.verbosity >= 1:
+                if args.returnr == True:
+                    print ("returning list with population and area (km^2) of {}".format(countryname))
+                    elist=[]
+                    for r in result:
+                        elist.append(r['population'])
+                        elist.append(r['area'])
+                    print (elist)
+                    return elist
+                else:
+                    for r in result:
+                        print (countryname, "'s population= ", r['population'])
+                        print (countryname, "'s area (km^2) = ", r['area'])
+            else:
+                if args.returnr == True:
+                    elist=[]
+                    for r in result:
+                        elist.append(r['population'])
+                        elist.append(r['area'])
+                    return elist
+                else:
+                    for r in result:
+                        print (r['population'])
+                        print (r['area'], " km^2")
+
+        elif args.extrainfo >= 1:
+            if args.verbosity >= 2:
+                if args.returnr == True:
+                    print ("succesfully fetched api data from REST Countries. returning population of {}".format(countryname))
+                    for r in result:
+                        return r['population']
+                else:
+                    print ("succesfully fetched api data from REST Countries")
+                    for r in result:
+                        print (countryname, "'s population = ", r['population'])
+            elif args.verbosity >= 1:
+                if args.returnr == True:
+                    print ("returning population of {}".format(countryname))
+                    for r in result:
+                        print (r['population'])
+                        return r['population']
+                else:
+                    for r in result:
+                        print (countryname, "'s populatio = ", r['population'])
+            else:
+                if args.returnr == True:
+                    for r in result:
+                        return r['population']
+                else:
+                    for r in result:
+                        print (r['population'])
+    else:
+        print ("sorry, something went wrong with the API request. check your internet connection and country/capital name and try again")
+        raise requests.exceptions.RequestException
--- userapp.py
+++ userapp.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-'''importing the module checks from capitalspackage. 
+'''importing the module checks from capitalspackage.
 inside checks module there are the functions load_csv,
 check_capital and check_state'''
 
@@ -11,9 +11,10 @@ from argparse import ArgumentParser
 parser = ArgumentParser()
 
 parser.add_argument("name", help = "write name of European country or capital", type= str.upper)
-parser.add_argument("-v", "--verbosity", help="incrementally increase output verbosity up to -vv", action="count", default=0)
-parser.add_argument("-r", "--returnr", help="returns result to store it, instead of printing it", action='store_true')
-parser.add_argument("-c", "--choosecsv", help="specify another csv file to use. make sure the format is CHINA;PECHINO/nGIAPPONE;TOKYO", action='store')
+parser.add_argument("-v", "--verbosity", help= "incrementally increase output verbosity up to -vv", action= "count", default= 0)
+parser.add_argument("-r", "--returnr", help= "returns result to store it, instead of printing it", action= 'store_true')
+parser.add_argument("-c", "--choosecsv", help= "specify another csv file to use. make sure the format is CHINA;PECHINO/nGIAPPONE;TOKYO", action= 'store')
+parser.add_argument("-e", "--extrainfo", help= "incrementally prints or returns (with -r) extra information about the country: population, area, currencies, languages", action= "count", default= 0)
 
 args = parser.parse_args()
 
@@ -25,3 +26,13 @@ list_of_capitals = checks.load_csv(filename)
 
 checks.check_capital(args.name, list_of_capitals, args)
 checks.check_state(args.name, list_of_capitals, args)
+
+if args.extrainfo > 0:
+    
+    base_url = "https://restcountries.eu/rest/v2/name/{}?fullText=true"
+    if args.name in list_of_capitals.keys():
+        checks.get_country_data(base_url, args.name, args)
+    else:
+        newargs= parser.parse_args([args.name, "--returnr"])
+        countryname= checks.check_state(args.name, list_of_capitals, newargs)
+        checks.get_country_data(base_url, countryname, args)
